1664abfe0fd14d7caa395f18fe476ad9af16ce96
Vinicius Martins <viniciusmartinssilva@live.com>
2024-03-06 10:06:07 -0300

    Add Button component and its types

---
+++ src/components/common/index.ts
@@ -0,0 +1,7 @@
+import Button from './Button'
+import FormInput from './FormInput'
+import Loading from './Loading'
+import Pagination from './Pagination'
+import SearchBar from './SearchBar'
+
+export { Button, FormInput, Pagination, SearchBar, Loading }
