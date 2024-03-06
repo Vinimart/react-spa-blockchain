ce96f6c66be869bf88d96c56794c594a2ee5cc74
Vinicius Martins <viniciusmartinssilva@live.com>
2024-03-06 10:09:38 -0300

    Add blockchain service and citizens service

---
+++ src/services/blockchain/index.ts
@@ -0,0 +1,4 @@
+import { initializeContract, initializeProvider } from './blockchainService'
+import { addCitizen, getAllCitizens } from './citizensService'
+
+export { initializeProvider, initializeContract, getAllCitizens, addCitizen }
