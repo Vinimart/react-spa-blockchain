1d501d8e93f6b98eb6c82c11af12656f3eab50db
Vinicius Martins <viniciusmartinssilva@live.com>
2024-03-07 15:24:06 -0300

    Update serve script to use port 8080

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
+  base: '/react-spa-blockchain/',
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
